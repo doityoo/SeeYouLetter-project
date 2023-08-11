601ed39edce68b5135679221bc57cdc44ab86ace
doityoo <yitsky@naver.com>
2023-08-11 19:21:22 +0900

    fix: eslint 설정 수정 - functions/.eslintrc.js 내에 룰 수정 -- quotes: [error, single] 따옴표 하나는 에러 표시
    -- object-curly-spacing: [error, always] 객체 앞뒤로 띄어쓰기 설정

--- SeeYouLetter/functions/.eslintrc.js
+++ SeeYouLetter/functions/.eslintrc.js
@@ -25,8 +25,9 @@ module.exports = {
     "import",
   ],
   rules: {
-    "quotes": ["error", "double"],
+    "quotes": ["error", "single"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "object-curly-spacing": ["error", "always"]
   },
 };
--- SeeYouLetter/functions/src/index.ts
+++ SeeYouLetter/functions/src/index.ts
@@ -1,20 +1,27 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from 'firebase-functions';
+import emailSender from '../../src/utils/emailSender';
+// -------------but '--jsx' is not set ERROR > emailSender확장명을 ts로 변경
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+export const sendEmail = functions.https.onRequest(async (req, res) => {
+  if (req.method === 'POST') {
+    const { toEmail, text, name, reservationDate, subject } = req.body;
+    console.log('서버 데이터 테스트(sendEmail): ', req.body);
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+    try {
+      // 여기에서 예약된 날짜 로직을 추가
+      // 예약된 날짜가 도달하면 이메일을 보내도록 설정
+      const delay = reservationDate.getTime() - Date.now();
+      if (delay > 0) {
+        await new Promise((resolve) => setTimeout(resolve, delay));
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
+        await emailSender(toEmail, name, text, reservationDate, subject);
+        res.status(200).json({ message: 'Email sent.' });
+      }
+    } catch (error) {
+      console.error('Error processing email:', error);
+      res.status(500).json({ message: 'Error processing email.' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed.' });
+  }
+});
