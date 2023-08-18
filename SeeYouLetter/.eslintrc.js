439af2e57bd806e8847ed196ff7de491d4dfe7c8
doityoo <yitsky@naver.com>
2023-08-18 14:27:46 +0900

    feat: seeYouLetter-server dir 추가 - node.js ts express nodemon 기본 서버 생성 - package.json, tsconfig.json 옵션 수정 - 로컬 서버 연결 확인

---
+++ SeeYouLetter/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        // "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
+        "prettier",
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": ["@typescript-eslint", "react", "jsx-a11y", "prettier"],
+    "rules": {
+        "quotes": ["error", "single"],
+        "import/no-unresolved": 0,
+        "indent": ["error", 2],
+        "object-curly-spacing": ["error", "always"],
+        "prettier/prettier": "error",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "jsx-a11y/anchor-is-valid": "off",
+        "jsx-a11y/alt-text": "warn",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { "ignoreRestSiblings": true }
+        ]
+    }
+}
