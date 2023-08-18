fb032124276a36332d0925712cb4326283d67248
doityoo <yitsky@naver.com>
2023-08-18 14:45:34 +0900

    fix: app.ts수정, emailSender.ts 추가 - app.ts 예약 메일 전송 로직으로 수정 - emailSender.ts firebase 인증 모듈 미설치,

--- seeYouLetter-server/src/app.ts
+++ seeYouLetter-server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import emailSender from './emailSender';
 
 const app = express();
 
@@ -8,14 +9,34 @@ type Data = {
 	url: string;
 };
 
-const sendData: Data = {
-	name: 'name',
-	age: 3,
-	url: 'tistory.com',
-};
+// const sendData: Data = {
+// 	name: 'name',
+// 	age: 3,
+// 	url: 'tistory.com',
+// };
 
-app.get('/get', (req: Request, res: Response) => {
-	res.send(sendData);
+app.post('/post', (req: Request, res: Response) => {
+	if (req.method === 'POST') {
+		const { toEmail, text, name, reservationDate, subject } = req.body;
+		console.log('서버 데이터 테스트(sendEmail): ', req.body);
+
+		try {
+			// 여기에서 예약된 날짜 로직을 추가
+			// 예약된 날짜가 도달하면 이메일을 보내도록 설정
+			const delay = reservationDate.getTime() - Date.now();
+			if (delay > 0) {
+				new Promise((resolve) => setTimeout(resolve, delay));
+
+				emailSender(toEmail, name, text, reservationDate, subject);
+				res.status(200).json({ message: 'Email sent.' });
+			}
+		} catch (error) {
+			console.error('Error processing email:', error);
+			res.status(500).json({ message: 'Error processing email.' });
+		}
+	} else {
+		res.status(405).json({ message: 'Method not allowed.' });
+	}
 });
 
 app.listen(8080);
---
+++ seeYouLetter-server/src/emailSender.ts
@@ -0,0 +1,107 @@
+import fs from 'fs';
+import path from 'path';
+import admin from 'firebase-admin';
+import handlebars from 'handlebars';
+import nodemailer from 'nodemailer';
+import schedule from 'node-schedule';
+import 'dayjs/locale/ko';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+// Firebase Admin SDK 초기화
+const serviceAccount = require('./../../seeyouletter-735f3-firebase-adminsdk.json');
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+
+const { EMAIL_SERVICE, EMAIL_USER, EMAIL_PASS } = process.env;
+
+interface MailOptionsTypes {
+	to: string;
+	subject: string;
+	html: string;
+}
+
+const emailSender = async (
+	toEmail: string,
+	name: string,
+	text: string,
+	reservationDate: Date,
+	subject: string
+) => {
+	try {
+		const templatePath = path.join(
+			process.cwd(),
+			'pages/api/email-template.hbs'
+		);
+		const templateSource = fs.readFileSync(templatePath, 'utf8');
+		const emailTemplate = handlebars.compile(templateSource);
+
+		const currentDate = dayjs(new Date()).format('YYYY년 MM월 DD일');
+
+		const mailTemplate = {
+			to: toEmail,
+			name: name,
+			subject: subject,
+			text: text,
+			currentDate: currentDate,
+		};
+
+		const emailContent = await emailTemplate(mailTemplate);
+
+		const mailOptions: MailOptionsTypes = {
+			to: toEmail,
+			subject: subject,
+			html: emailContent,
+		};
+
+		// firebase db에 mailOptions 저장
+		const saveMailOptionsToFirestore = async (
+			mailOptions: MailOptionsTypes
+		) => {
+			try {
+				await db.collection('mailOptions').add(mailOptions);
+				console.log('mailOptions 데이터가 Firestore에 저장되었습니다.');
+			} catch (error) {
+				console.error(
+					'Firestore에 데이터를 저장하는 중 오류가 발생했습니다:',
+					error
+				);
+			}
+		};
+
+		await saveMailOptionsToFirestore(mailOptions);
+
+		// 예약된 날짜가 도달하면 이메일을 보내도록 설정
+		const transporter = await nodemailer.createTransport({
+			service: EMAIL_SERVICE,
+			port: 587,
+			secure: false,
+			auth: {
+				user: EMAIL_USER,
+				pass: EMAIL_PASS,
+			},
+		});
+
+		// 예약된 날짜에 이메일을 보내도록 스케줄링
+		const scheduledJob = schedule.scheduleJob(reservationDate, async () => {
+			try {
+				await transporter.sendMail(mailOptions);
+				console.log('예약 편지 보내기 성공');
+			} catch (error) {
+				console.error('이메일 처리 중 오류가 발생했습니다(1):', error);
+			} finally {
+				// 작업이 실행된 이후에 해당 작업을 삭제합니다.
+				console.log('scheduled end!');
+			}
+		});
+	} catch (error) {
+		console.error('이메일 처리 중 오류가 발생했습니다(2):', error);
+	}
+};
+
+export default emailSender;
