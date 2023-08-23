67cafb4645bcc731cf39f0c7847b09013fe88c89
doityoo <yitsky@naver.com>
2023-08-23 14:38:27 +0900

    feat: emailSender.ts sdk import 수정

--- seeYouLetter-server/src/emailSender.ts
+++ seeYouLetter-server/src/emailSender.ts
@@ -12,19 +12,22 @@ dayjs.locale('ko');
 dayjs.extend(relativeTime);
 
 // Firebase Admin SDK 초기화
-// const serviceAccount = require('./../seeyouletter-735f3-firebase-adminsdk.json');
-require('dotenv').config();
-const firebaseAdminSdkFile = process.env.FIREBASE_ADMIN_SDK_FILE;
-console.log('FIREBASE_ADMIN_SDK_FILE:', process.env.FIREBASE_ADMIN_SDK_FILE);
+const serviceAccount = require('./../seeyouletter-735f3-firebase-adminsdk.json');
+// require('dotenv').config();
+// const firebaseAdminSdkFile = process.env.FIREBASE_ADMIN_SDK_FILE;
+// console.log('FIREBASE_ADMIN_SDK_FILE:', process.env.FIREBASE_ADMIN_SDK_FILE);
 
-if (!firebaseAdminSdkFile) {
-	console.error('FIREBASE_ADMIN_SDK_FILE이 정의되지 않았습니다.');
-	process.exit(1);
-}
+// if (!firebaseAdminSdkFile) {
+// 	console.error('FIREBASE_ADMIN_SDK_FILE이 정의되지 않았습니다.');
+// 	process.exit(1);
+// }
 
 // Firebase Admin SDK 초기화
+// admin.initializeApp({
+// 	credential: admin.credential.cert(firebaseAdminSdkFile),
+// });
 admin.initializeApp({
-	credential: admin.credential.cert(firebaseAdminSdkFile),
+	credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
