641762971eb706d8dfc5994acfedcf3d32e8969d
doityoo <yitsky@naver.com>
2023-08-24 19:08:21 +0900

    feat: 예약 메일 로직 중복으로 삭제(delay 변수)

--- seeYouLetter-server/src/index.ts
+++ seeYouLetter-server/src/index.ts
@@ -5,15 +5,10 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // Body 데이터 파싱을 위한 미들웨어 추가
+app.use(express.json());
 
-// type Data = {
-// 	name: string;
-// 	age: number;
-// 	url: string;
-// };
-
-app.get('/letterForm', (res: Response) => {
+// s3에서 get 요청 응답 여부 테스트
+app.get('/letterForm', (req: Request, res: Response) => {
 	try {
 		console.log(res.json('server hello'));
 		res.status(200).json('get response');
@@ -29,15 +24,8 @@ app.post('/sendEmail', (req: Request, res: Response) => {
 		res.status(200).json({ request: req.body }); // req body 잘등어오는지 테스트 코드
 
 		try {
-			// 여기에서 예약된 날짜 로직을 추가
-			// 예약된 날짜가 도달하면 이메일을 보내도록 설정
-			const delay = reservationDate.getTime() - Date.now();
-			if (delay > 0) {
-				new Promise((resolve) => setTimeout(resolve, delay));
-
-				emailSender(toEmail, name, text, reservationDate, subject);
-				res.status(200).json({ message: 'Email sent.' });
-			}
+			emailSender(toEmail, name, text, reservationDate, subject);
+			res.status(200).json({ message: 'Email sent.' });
 		} catch (error) {
 			console.error('Error processing email:', error);
 			res.status(500).json({ message: 'Error processing email.' });
@@ -45,7 +33,6 @@ app.post('/sendEmail', (req: Request, res: Response) => {
 	} else {
 		res.status(405).json({ message: 'Method not allowed.' });
 	}
-	// res.status(200).json({ message: 'success' });
 });
 
 const port = 8080;
