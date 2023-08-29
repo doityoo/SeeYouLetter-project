523a0a1de46e9800192a46eeafe60f8deabd27e6
doityoo <yitsky@naver.com>
2023-08-29 12:19:36 +0900

    fix: 에러로 인한 코드 수정

--- seeYouLetter-server/src/index.ts
+++ seeYouLetter-server/src/index.ts
@@ -7,21 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// s3에서 get 요청 응답 여부 테스트
-app.get('/letterForm', (req: Request, res: Response) => {
-	try {
-		console.log(res.json('server hello'));
-		res.status(200).json('get response');
-	} catch (err) {
-		res.status(500).json('get response error');
-	}
-});
-
 app.post('/sendEmail', (req: Request, res: Response) => {
 	if (req.method === 'POST') {
 		const { toEmail, text, name, reservationDate, subject } = req.body;
 		console.log('서버 데이터 테스트(sendEmail): ', req.body);
-		res.status(200).json({ request: req.body }); // req body 잘등어오는지 테스트 코드
 
 		try {
 			emailSender(toEmail, name, text, reservationDate, subject);
