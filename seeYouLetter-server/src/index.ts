12f2a4ad543aca928ce8d6299873638a68f288d0
doityoo <yitsky@naver.com>
2023-08-29 15:51:23 +0900

    배포 에러 테스트(8)

--- seeYouLetter-server/src/index.ts
+++ seeYouLetter-server/src/index.ts
@@ -13,9 +13,12 @@ app.post('/sendEmail', async (req: Request, res: Response) => {
 		console.log('서버 데이터 테스트(sendEmail): ', req.body);
 
 		// 클라이언트에게 즉시 응답을 보냄
-		await res.status(200).json({ message: 'Email will be sent.' });
-		await emailSender(toEmail, name, text, reservationDate, subject);
-		return;
+		try {
+			await emailSender(toEmail, name, text, reservationDate, subject);
+			res.status(200).json({ message: '이메일이 전송될 예정입니다.' });
+		} catch (error) {
+			res.status(500).json({ message: '이메일 전송 중 오류가 발생했습니다.' });
+		}
 	} else {
 		return res.status(405).json({ message: 'Method not allowed.' });
 	}
